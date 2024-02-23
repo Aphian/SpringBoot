342e6c583edba5e591b1ad89a07e86bfe7c2fd80
Sang <rsw7876@naver.com>
2024-02-23 21:18:18 +0900

    Springboot

--- SBB/src/main/java/com/example/SBB/QuestionRepository.java
+++ SBB/src/main/java/com/example/SBB/QuestionRepository.java
@@ -1,8 +1,12 @@
 package com.example.SBB;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
+	// 사용할 메서드 작성
 	Question findBySubject(String subject);
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
 
 }
--- SBB/src/test/java/com/example/SBB/SbbApplicationTests.java
+++ SBB/src/test/java/com/example/SBB/SbbApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.SBB;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // 목록 조회
 import java.util.List;
@@ -12,12 +13,16 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
 	
 	@Autowired
 	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
 	
 	// 입력 Create
 	@Test
@@ -55,10 +60,89 @@ class SbbApplicationTests {
 		}
 	}
 	
+	// findBySubject 메서드 조회
 	@Test
 	void testJpa4() {
 		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(1, q.getId());
+	}
+	
+	
+	// findBySubjectAndContent 메서드 조쇠
+	@Test
+	void testJpa5() {
+		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+	
+	// 특정 문자열 포함 조회
+	@Test
+	void testJpa6() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+	
+	// 데이터 수정
+	@Test
+	void testJpa8() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+	
+	// 데이터 삭제
+	@Test
+	void testJpa9() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
+	
+	// 객체애 대한 답변 작성
+	@Test
+	void testJpa10() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		Answer a = new Answer();
+		a.setContent("`네 자동으로 생성됩니다.");
+		// 어떤 질문의 답변인지 객체가 필요함
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+	
+	// 답변데이터 조회
+	@Test
+	void testJpa11() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
+	
+	// 질문에 대한 잡변 조회
+	// 테스트 코드에서 오류가 발생할 수 있음 -> DB 세션이 끊어지기 때문.
+	// 실제로 서버에서는 발생하지 않는다.
+	// 테스트 코드에서 오류를 해결하기 위해 Transactional 애너테이션 사용
+	@Transactional
+	@Test
+	void testJap12() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		List<Answer> answerList = q.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 
 	@Test
