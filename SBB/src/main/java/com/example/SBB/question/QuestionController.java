63a7e46122b6ad55a5b506eeb661963c5a351f4e
Aphian <rsw7876@naver.com>
2024-02-28 22:31:48 +0900

    Springboot

--- SBB/src/main/java/com/example/SBB/question/QuestionController.java
+++ SBB/src/main/java/com/example/SBB/question/QuestionController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -17,7 +19,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 	
 	// URL 매핑
-	@GetMapping("/question/list")
+	// @GetMapping("/question/list")
+	// Prefix 설정
+	@GetMapping("/list")
 	public String list(Model model) {
 		// List<Question> questionList = this.questionRepository.findAll();
 		List<Question> questionList = this.questionService.getList();
@@ -26,7 +30,8 @@ public class QuestionController {
 	}
 
 	// 상세 페이지
-	@GetMapping("/question/detail/{id}")
+	// @GetMapping("/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model.model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
